be32404b89e497882af2881ceeec3c9cdd480e66
CIPHERTron <pritish.samal918@gmail.com>
2021-04-07 22:12:52 +0530

    Add routes | /, /update, /delete, /about

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todos.db"
@@ -18,12 +19,46 @@ class Todo(db.Model):
         return f"{self.sno} - {self.title}"
 
 
-@app.route('/')
-def Home():
-    todo = Todo(title="First todo", desc="It's just fpr testing!")
-    db.session.add(todo)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
     db.session.commit()
-    return render_template('index.html')
+    return redirect('/')
 
 
 if __name__ == '__main__':
