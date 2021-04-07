7dec8dd27f21ac93c2791ef990836b5f5f5ade82
CIPHERTron <pritish.samal918@gmail.com>
2021-04-07 13:04:58 +0530

    Config SQLALCHEMY for sqlite db

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todos.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# class Todo(db.Model):
+#         sno = db.column
 
 
 @app.route('/')
