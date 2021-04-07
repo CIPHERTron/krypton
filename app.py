e70643e3cbc5c63659f9a0123870ee73bf3b7f7c
CIPHERTron <pritish.samal918@gmail.com>
2021-04-07 14:06:01 +0530

    Add ToDo model class

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todos.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# class Todo(db.Model):
-#         sno = db.column
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
 
 
 @app.route('/')
-def dashboard():
+def Home():
+    todo = Todo(title="First todo", desc="It's just fpr testing!")
+    db.session.add(todo)
+    db.session.commit()
     return render_template('index.html')
 
 
